a6b103502559594cfde842d8cfeaf7c5efb01a2d
pieroVG <piero.vega-gutierrez@univ-tlse3.fr>
2024-01-19 09:32:58 +0100

    ajout class Romain, recevoirCoup et frapper

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -9,12 +9,17 @@ public class Gaulois {
 		this.force = force;
 	}
 	
-	public String getNom () {
+	 public String getNom () {
 		return nom;
 	}
 	
 	public void parler(String texte) {
-		System.out.println("Le gaulois " + nom + " : \" " + texte +" \" " );
+		System.out.println("Le gaulois " + nom + " : \"" + texte +"\"" );
+	}
+	
+	public void frapper(Romain romain) {
+		System.out.println( nom + " envoie un grand coup dans la mâchoire de " + romain.getNom() );
+		romain.recevoirCoup(force/3);
 	}
 	
 	public static void main(String[] args) {
--- src/test_fonctionnel/TestGaulois.java
+++ src/test_fonctionnel/TestGaulois.java
@@ -1,10 +1,25 @@
 package test_fonctionnel;
 
 import personnages.Gaulois;
+import personnages.Romain;
 
 public class TestGaulois {
 	public static void main(String[] args) {
+		
+		
 		Gaulois asterix = new Gaulois("Astérix",8);
-		asterix.parler("amongus");
+		Romain minus = new Romain ("Minus",6);
+		
+		System.out.println(asterix.getNom());
+		asterix.parler("Bonsoir");
+		minus.parler("Un Gau .. Gaugole");
+		
+		/*for (int i = 0; i<2; i++) {
+			minus.recevoirCoup(3);
+		}*/
+		
+		for (int i = 0; i<3; i++) {
+			asterix.frapper(minus);
+		}
 	}
 }
