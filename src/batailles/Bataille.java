50a3c1ae923d9de443b44fa29a80d7b309d3a968
pieroVG <piero.vega-gutierrez@univ-tlse3.fr>
2024-02-29 08:37:27 +0100

    ajout interface Bataille

---
+++ src/batailles/Bataille.java
@@ -0,0 +1,9 @@
+package batailles;
+
+public interface Bataille {
+	public void decrireContexte();
+	public void choisirCombattants();
+	public void preparerCombat();
+	public void decrireCombat();
+	public void donnerResultat();
+}
--- src/batailles/Conteur.java
+++ src/batailles/Conteur.java
@@ -1,5 +1,30 @@
 package batailles;
 
-public class Conteur {
-
+public class Conteur implements Bataille {
+	private String nom;
+	
+	public Conteur(String nom) {
+		this.nom = nom;
+	}
+	
+	public void decrireContexte() {
+		
+	}
+	
+	public void choisirCombattants() {
+		
+	}
+	
+	public void preparerCombat() {
+		
+	}
+	
+	public void decrireCombat() {
+		
+	}
+	
+	public void donnerResultat() {
+		
+	}
+	
 }
